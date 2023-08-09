ddc8f5eba67f1cce53585694ba9a43568e4507a3
Gustavo Sicaja <gustavo.sicaja@ssasa.com>
2023-08-09 10:55:48 -0600

    Creando table y mostrar la tabla y automatizar el mostrar la listas

--- mvcCapas/mvcCapas/Scripts/generic.js
+++ mvcCapas/mvcCapas/Scripts/generic.js
@@ -1,1 +1,34 @@
 ﻿
+function pintar(objConfiguratcion){
+
+
+    fetch(objConfiguratcion.url)
+
+        .then(res => res.json())
+        .then(res => {
+            var contenido = "<table class='table'>";
+            contenido += "<tr>"
+
+            for (var j = 0; j < objConfiguratcion.cabeceras.length; j++) {
+
+                contenido += "<th>"+objConfiguratcion.cabeceras[j]+"</th>"
+            }
+            var fila;
+            var propiedadActual;
+            for (var i = 0; i < res.length; i++) {
+                fila = res[i]
+                
+                contenido += "<tr>"
+                for (var j = 0; j < objConfiguratcion.propiedades.length; j++) {
+                    propiedadActual = objConfiguratcion.propiedades[j];
+                    contenido += "<td>" + fila[propiedadActual] + "</td>"
+                }
+             
+                contenido += "</tr>"
+            }
+
+            contenido += "</table>"
+            document.getElementById(objConfiguratcion.id).innerHTML = contenido;
+            //alert(res)
+        })
+}
--- mvcCapas/mvcCapas/Scripts/tipoHabitacion.js
+++ mvcCapas/mvcCapas/Scripts/tipoHabitacion.js
@@ -3,29 +3,13 @@
 }
 
 function listarTipoHabtiacion() {
-    fetch("TipoHabitacion/Lista")
 
-        .then(res => res.json())
-        .then(res => {
-            var contenido = "<table class='table'>";
-            contenido += "<tr>"
-            contenido += "<th>ID</th>"
-            contenido += "<th>NOMBRE</th>"
-            contenido += "<th>DESCRIPCION</th>"
-            contenido += "</tr>"
+    pintar({
+        url: "TipoHabitacion/Lista",
+        id: "divTabla",
+        cabeceras: ["Id", "Nombre", "Descripcion"],
+        propiedades: ["id", "nombre", "descripcion"]
+    })
 
-            for (var i = 0; i < res.length; i++) {
-                fila = res[i]
-
-                contenido += "<tr>"
-                contenido += "<td>"+fila.id+"</td>"
-                contenido += "<td>" + fila.nombre+"</td>"
-                contenido += "<td>"+fila.descripcion+"</td>"
-                contenido += "</tr>"
-            }
-
-            contenido +="</table>"
-            document.getElementById("divTabla").innerHTML = contenido;
-           //alert(res)
-        })
+   
 }
