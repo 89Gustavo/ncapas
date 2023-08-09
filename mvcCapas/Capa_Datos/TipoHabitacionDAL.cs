f5df2a59614f98ea0a06a474a2e89eb7857616c0
Gustavo Sicaja <gustavo.sicaja@ssasa.com>
2023-08-09 11:22:35 -0600

    primera conexion a base de datos sql y c# metodo procedure

--- mvcCapas/Capa_Datos/TipoHabitacionDAL.cs
+++ mvcCapas/Capa_Datos/TipoHabitacionDAL.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 using Capa_Entidad;
 
@@ -10,28 +12,71 @@ namespace Capa_Datos
 {
     public class TipoHabitacionDAL
     {
-        public List<TipoHabitacionCLS> listarrTipoHabitacion(){
+        /*        public List<TipoHabitacionCLS> listarrTipoHabitacion(){
 
-            List<TipoHabitacionCLS> lista = new List<TipoHabitacionCLS>();
+                    List<TipoHabitacionCLS> lista = new List<TipoHabitacionCLS>();
 
 
-            lista.Add(new TipoHabitacionCLS
+                    lista.Add(new TipoHabitacionCLS
+                    {
+                        id = 1,
+                        nombre= "Simple",
+                        descripcion= "Solo para uno"
+
+                    });
+                    lista.Add(new TipoHabitacionCLS
+                    {
+                        id = 2,
+                        nombre = "Doble",
+                        descripcion = "Solo para casados"
+
+                    });
+
+
+                    return lista;
+                }*/
+
+        public List<TipoHabitacionCLS> listarrTipoHabitacion()
+        {
+            List<TipoHabitacionCLS> lista = null;
+
+            using (SqlConnection cn = new SqlConnection("Data Source=LP-OP-03;Initial Catalog=BDHotel;Persist Security Info=True;User ID=sa;Password=Fantasmita@0608")) 
             {
-                id = 1,
-                nombre= "Simple",
-                descripcion= "Solo para uno"
 
-            });
-            lista.Add(new TipoHabitacionCLS
-            {
-                id = 2,
-                nombre = "Doble",
-                descripcion = "Solo para casados"
+                try
+                {
+                    cn.Open();
+                    
+                    
+                    using (SqlCommand cmd = new SqlCommand("uspListarTipoHabitacion", cn)) { 
+                       // buena practica indicar que prcedure
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        
+                        SqlDataReader drd = cmd.ExecuteReader();
+                        TipoHabitacionCLS oTipoHabitacionCLS;
+                       
+                        if (drd != null) {
+                            lista = new List<TipoHabitacionCLS>();
+                            while (drd.Read()) {
+                                oTipoHabitacionCLS = new TipoHabitacionCLS();
+                                oTipoHabitacionCLS.id=drd.GetInt32(0);
+                                oTipoHabitacionCLS.nombre = drd.GetString(1);
+                                oTipoHabitacionCLS.descripcion = drd.GetString(2);
 
-            });
+                                lista.Add(oTipoHabitacionCLS);
+                            }
+                        }
+                    }
 
 
-            return lista;
+                    cn.Close();
+                }
+                catch(Exception ex) {
+                    cn.Close();
+                }
+            }
+
+                return lista;
         }
     }
 }
